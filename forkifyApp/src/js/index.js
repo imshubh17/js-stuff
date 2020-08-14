72a761265599c5bcefaa4deec430c25dc55f36c2
imshubh17 <sss.1771997@gmail.com>
2020-08-14 19:03:44 +0530

    add first api call

--- forkifyApp/src/js/index.js
+++ forkifyApp/src/js/index.js
@@ -1,1 +1,12 @@
-// Global app controller
+import axios from 'axios';
+
+async function getResult(query){
+    try{
+        const result = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        console.log(result);
+    } catch(err) {
+        alert(err);
+    }
+}
+
+getResult('pizza');

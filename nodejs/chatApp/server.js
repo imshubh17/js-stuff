107949ef098299b72a5cc3a854b95eb65e56f2d7
imshubh17 <sss.1771997@gmail.com>
2020-08-19 14:13:17 +0530

    created basic chat app

--- nodejs/chatApp/server.js
+++ nodejs/chatApp/server.js
@@ -1,7 +1,23 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+
+var messages = []
+
+app.get('/messages',(req,res) =>{
+    res.send(JSON.stringify(messages))
+})
+
+app.post('/messages',(req,res) =>{
+    console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
+})
+
+
 var server = app.listen(3000,()=>{
     console.log(`server in listening ${server.address().port}`)
 })

02a43d3174198ca87169180638f28b4b88c91425
imshubh17 <sss.1771997@gmail.com>
2020-08-21 13:18:50 +0530

    express setup

---
+++ nodejs/building-website-nodejs-express/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { request } = require('http');
+const { response } = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (request,response)=>{
+    response.send('hello express :)');
+});
+
+app.listen(port,()=>{
+    console.log(`express server listening on port ${port}`);
+})

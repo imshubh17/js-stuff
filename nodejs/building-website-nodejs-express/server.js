40e86a06e7ca5f4e662414ea3eec7207b98a20ce
imshubh17 <sss.1771997@gmail.com>
2020-08-21 15:04:06 +0530

    serving html page and static content

--- nodejs/building-website-nodejs-express/server.js
+++ nodejs/building-website-nodejs-express/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const { request } = require('http');
-const { response } = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'./static'))); //middleware
+
 app.get('/', (request,response)=>{
-    response.send('hello express :)');
+    response.sendFile(path.join(__dirname,'./static/index.html'));
+});
+
+app.get('/speakers', (request,response)=>{
+    response.sendFile(path.join(__dirname,'./static/speakers.html'));
 });
 
 app.listen(port,()=>{

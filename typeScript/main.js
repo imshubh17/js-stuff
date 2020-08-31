fe4220226fd95962fef70c0a0459cf035215c0d1
Shubham <sss.1771997@gmail.com>
2020-08-31 10:07:38 +0530

    typescript setup, variables types

---
+++ typeScript/main.js
@@ -0,0 +1,18 @@
+// let message = 'hello TS';
+// console.log(message);
+// *** variabless
+// let sum : Number = 0;
+// console.log(sum);
+// sum = 5;
+// console.log(sum);
+// let sentance : string = `hiii I'm pc`;
+// console.log(sentance);
+// let list1 : number[] = [1,2,3,4,5];
+// let list2 : any[] = [1, 'hiii', true]
+// let person : [string , number] = ['shubham' , 23];
+// enum Color {red, blue , black};
+// let c: Color = Color.black
+// console.log(c);
+var varUnknown = 10;
+varUnknown = 'shubham';
+console.log(varUnknown.toUpperCase());

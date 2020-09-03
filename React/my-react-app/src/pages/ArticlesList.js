0f215bd81f317673e5ca624439f77c252d941221
Shubham <sss.1771997@gmail.com>
2020-09-03 16:24:39 +0530

    updated articleslist page

--- React/my-react-app/src/pages/ArticlesList.js
+++ React/my-react-app/src/pages/ArticlesList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import articles from './article-content';
+import {Link} from 'react-router-dom';
 
 const ArticlesList = ()=>{
     return (
         <>
             <h1>ArticleList</h1>
+            {articles.map((article,key)=>(
+                <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0,150)}...</p>
+                </Link>
+
+            ))}
         </>
         
     )

4dd7c9576589ab25939d7f8ca6bc86afebbb02d2
Shubham <sss.1771997@gmail.com>
2020-09-03 16:12:12 +0530

    updated article page

--- React/my-react-app/src/pages/ArticlePage.js
+++ React/my-react-app/src/pages/ArticlePage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import articles from './article-content';
 
-const ArticlePage = ()=>{
+
+const ArticlePage = ({match})=>{
+    const name = match.params.name;
+    const article = articles.find(article => article.name === name);
+
+    if (!article) return <h1>Article not Available</h1>;
+
     return (
         <>
-            <h1>ArticlePage</h1>
+            <h1>{article.title}</h1>
+            {article.content.map((para,key) => (
+                <p key = {key}>{para}</p>
+            ))}
         </>
         
     )

4c61ce528bff81e7667832dcfb6f8231f17c53ad
Shubham <sss.1771997@gmail.com>
2020-09-04 14:15:47 +0530

    added useEffect

--- React/my-react-app/src/pages/ArticlePage.js
+++ React/my-react-app/src/pages/ArticlePage.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({match})=>{
+
     const name = match.params.name;
     const article = articles.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvote:0 , comments: [] })
+    useEffect(()=>{
+        setArticleInfo({ upvote: 3})
+    },[name]);//empty for effect when load and then name changes
 
     if (!article) return <NotFoundPage/>;
 
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This Post is upvoted {articleInfo.upvote}</p>
             {article.content.map((para,key) => (
                 <p key = {key}>{para}</p>
             ))}
